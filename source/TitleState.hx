7ebb31531c79055ce2bc5cf2812256cca2e0f933
PlasmaOJ15 <orlandorodriquezjr@gmail.com>
2023-07-19 15:03:05 -0700

    Added Title Screen Code

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -307,8 +307,9 @@ class TitleState extends MusicBeatState
 				gfDance.animation.addByIndices('danceLeft', 'BB Title Bump', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], "", 24, false);
 				gfDance.animation.addByIndices('danceRight', 'BB Title Bump', [27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "", 24, false);
 			case 'HAPPY':
-				PlayState.SONG = Song.loadFromJson('test', 'test')
+				PlayState.SONG = Song.loadFromJson('test', 'test');
 				LoadingState.loadAndSwitchState(new PlayState());
+				FlxG.save.data.psychDevsEasterEgg = '';
 			#end
 
 			default:
